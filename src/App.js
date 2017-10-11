9eba2489e995c07c2aa36cd7212efeb94b841a91
Ahkeem Lang <langahkeem@gmail.com>
2017-10-10 19:44:52 -0500

    added the index.html file to my app.js file so I can receive a template of what I want the app to look like

--- src/App.js
+++ src/App.js
@@ -5,14 +5,65 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div class="App">
+        <header>
+          <h1>RSVP</h1>
+          <p>Reserve App</p>
+          <form>
+              <input type="text" value="Safia" placeholder="Invite Someone" />
+              <button type="submit" name="submit" value="submit">Submit</button>
+          </form>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div class="main">
+          <div>
+            <h2>Invitees</h2>
+            <label>
+              <input type="checkbox" /> Hide those who have not responded
+            </label>
+          </div>
+          <table class="counter">
+            <tbody>
+              <tr>
+                <td>Attending:</td>
+                <td>2</td>
+              </tr>
+              <tr>
+                <td>Unconfirmed:</td>
+                <td>1</td>
+              </tr>
+              <tr>
+                <td>Total:</td>
+                <td>3</td>
+              </tr>
+            </tbody>
+          </table>
+          <ul>
+            <li class="pending"><span>Safia</span></li>
+            <li class="responded"><span>Iver</span>
+              <label>
+                <input type="checkbox" checked /> Confirmed
+              </label>
+              <button>edit</button>
+              <button>remove</button>
+            </li>
+            <li class="responded">
+              <span>Corrina</span>
+              <label>
+                <input type="checkbox" checked /> Confirmed
+              </label>
+              <button>edit</button>
+              <button>remove</button>
+            </li>
+            <li>
+              <span>Joel</span>
+              <label>
+                <input type="checkbox" /> Confirmed
+              </label>
+              <button>edit</button>
+              <button>remove</button>
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
