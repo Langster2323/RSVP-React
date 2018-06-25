0e72fcff4f0af149d7ccd793f96c1b759e0e910e
Ahkeem Lang <langahkeem@gmail.com>
2018-06-25 10:05:09 -0500

    Appropriately passed the pendingguest prop

--- src/App.js
+++ src/App.js
@@ -140,7 +140,8 @@ getTotalInvited = () => this.state.guests.length;
           toggleEditingAt={this.toggleEditingAt}
           setNameAt={this.setNameAt}
           isFiltered={this.state.isFiltered}
-          removeGuestAt={this.removeGuestAt} />
+          removeGuestAt={this.removeGuestAt}
+          pendingGuest={this.state.pendingGuest} />
         </div>
       </div>
     );
